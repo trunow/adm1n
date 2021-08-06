8901c0b59a9b8e5f28c0f0c1e8dd4ec571e57e3f
trunow <slava@trunov.me>
2021-08-06 12:29:10 +0300

    majQ

--- utils/dom.js
+++ utils/dom.js
@@ -1,5 +1,19 @@
-// типа jQ
-const $ = (q) => document.querySelector(q);
-$.meta = (n) => ($('meta[name="' + n + '"') || {})?.content;
+// 🚀 majQ.js (minimal another jQuery)
+const $ = (s,p) => (p instanceof EventTarget ? p : document).querySelector(s);
+$.win=$.W=window;$.doc=$.D=document;$.head=$.H=$.D.head;$.body=$.B=$.D.body;$.ET = EventTarget;
+$._ = $.prototype = $.ET.prototype;
+$.isET = is => is instanceof $.ET;
+$.isETor = (is,or) => $.isET(is)?is:$.isETor(or,$.D);
+$.all = $.$ = (s,p) => $.isETor(p).querySelectorAll(s);
+$.create = tag => $.D.createElement(tag);
+$._.on = addEventListener;
+$._.off = removeEventListener;
+$._.push = function(tag){let el=$.create(tag);this.appendChild(el);return el;}
+$._.text = function(txt){if(!txt) return this.innerText;this.innerText=txt;return this;} // TODO textContent
+$._.html = function(html){if(!html) return this.innerHTML;this.innerHTML=html;return this;}
+$._.css = function (css) { if (!css) return this.style.cssText; this.style.cssText = css; return this; }
+$.meta = (n) => $(`meta[name="${n}"]`)?.content;
+
+window.$ = $;
 
 export default $;
